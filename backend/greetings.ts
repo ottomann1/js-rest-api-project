ce462c545b4c0b870a57c440e6f57d80cf6a8c03
Yellik <yelenakensborn@gmail.com>
2024-07-03 14:03:05 +0200

    added get greetings Co-authored-by: Nils Hansson <nilsmenghansson@gmail.com> Co-authored-by: ottomann1 <otto.kostmann@hotmail.com>

---
+++ backend/greetings.ts
@@ -0,0 +1,10 @@
+const data = {
+    {
+        "id": "1";
+        "greetings": "Hey, how is it going?"
+    },
+    {
+        "id": "2",
+        "greetings": "This is another greeting?"
+    }
+}
--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,27 @@
-import express from 'express'
-const app = express()
-const port = 3000
+import express from "express";
+const app = express();
+const port = 3000;
 
-app.get('/status', (req, res) => {
+
+
+let greetings = [];
+
+class Greeting {
+  id: string;
+  greeting: string;
+  constructor(id: string, greeting: string) {
+    this.id = id;
+    this.greeting = greeting;
+  }
+}
+
+app.get("/status", (req, res) => {
   res.sendStatus(200);
-})
+});
 
+app.get("/api/v1/greetings", (req, res) => {
+  res.json(greetings)
+}), 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
