73c71d68e0c930db3f86eb3c8f1ba5ca48568ef9
Otto Bossmann <otto.kostmann@hotmail.com>
2024-07-04 12:07:51 +0200

    create form for user
    Co-authored-by: Nils Hansson <nilsmenghansson@gmail.com>
    Co-authored-by: Yellik <yelenakensborn@gmail.com>

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,4 @@
-import "./style.css";
+
 
 const fetchGreetings = () => {
   fetch("http://localhost:3000/api/v1/greetings")
@@ -24,34 +24,38 @@ const fetchGreetings = () => {
 };
 fetchGreetings();
 
-// Get the modal
-const modal = document.getElementById("myModal") as HTMLDivElement;
-
-// Get the button that opens the modal
-const btn = document.getElementById("myBtn") as HTMLButtonElement;
-
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0] as HTMLSpanElement;
-
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
+const greetingModal = document.getElementById("greetingModal") as HTMLDivElement;
+const greetingBtn = document.getElementById("greetingBtn") as HTMLButtonElement;
+const greetingSpan = document.getElementById("greetingClose") as HTMLSpanElement;
+greetingBtn.onclick = function () {
+  greetingModal.style.display = "block";
 };
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
+greetingSpan.onclick = function () {
+  greetingModal.style.display = "none";
 };
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == greetingModal) {
+    greetingModal.style.display = "none";
   }
 };
 
-function handleFormSubmit(event: Event) {
-  event.preventDefault(); // Prevent the default form submission behavior
+const userModal = document.getElementById("userModal") as HTMLDivElement;
+const userBtn = document.getElementById("userBtn") as HTMLButtonElement;
+const userSpan = document.getElementById("userClose") as HTMLSpanElement;
+userBtn.onclick = function () {
+  userModal.style.display = "block";
+};
+userSpan.onclick = function () {
+  userModal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == userModal) {
+    userModal.style.display = "none";
+  }
+};
+
+function handleGreetingFormSubmit(event: Event) {
+  event.preventDefault();
   const nameInput = document.getElementById(
     "greetingInput"
   ) as HTMLInputElement;
@@ -70,8 +74,30 @@ function handleFormSubmit(event: Event) {
       console.error("Error:", error);
     });
 }
+function handleUserFormSubmit(event: Event) {
+  event.preventDefault();
+  const nameInput = document.getElementById(
+    "usernameInput"
+  ) as HTMLInputElement;
+  const nameValue = nameInput.value;
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username: nameValue }),
+  };
+  fetch("http://localhost:3000/api/v1/user", requestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log({ data });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("greetingsForm") as HTMLFormElement;
-  form.addEventListener("submit", handleFormSubmit);
+  const greetingsForm = document.getElementById("greetingsForm") as HTMLFormElement;
+  greetingsForm.addEventListener("greetingSubmit", handleGreetingFormSubmit);
+  const userForm = document.getElementById("userForm") as HTMLFormElement;
+  userForm.addEventListener("userSubmit", handleUserFormSubmit);
 });
